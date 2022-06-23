d966387dc8b73c9cb8ea2dd6bb3c984dfb2b88ec
•JINN• <95747858+alvinbaby@users.noreply.github.com>
2022-06-23 10:42:04 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,49 +2,32 @@
 
 clear
 echo -e "
-                
-    _____  _____  ____  _____  ____  _____      _______          _     _________  
-   |_   _||_   _||_   \|_   _||_   \|_   _|    |_   __ \        / \   |  _   _  | 
-     | |    | |    |   \ | |    |   \ | | ______ | |__) |      / _ \  |_/ | | \_| 
- _   | |    | |    | |\ \| |    | |\ \| ||______||  __ /      / ___ \     | |     
-| |__' |   _| |_  _| |_\   |_  _| |_\   |_      _| |  \ \_  _/ /   \ \_  _| |_    
-`.____.'  |_____||_____|\____||_____|\____|    |____| |___||____| |____||_____|  [V1.0]
-
 ___________________________________________₹
-     Coding master : LizOrd                    
-  [ Telegram : https://t.me/lizord2059 ]            
+     Coding master : LizOrd
+  [ Telegram : https://t.me/lizord2059 ]
 ___________________>=______________________₹
-     Repository master : MR JINN        
-  [ Telegram : https://t.me/MrJinnTelegram ] 
+     Repository master : MR JINN
+  [ Telegram : https://t.me/MrJinnTelegram ]
 ___________________________________________₹
 
 "
-
 echo "Please wait..."
 cd $HOME
 cd .termux
 cp colors.properties ../;rm colors.properties
 wget https://raw.githubusercontent.com/alvinbaby/Terminal-In-Termux/main/alvinbaby/colors.properties
-
+clear
 cd $HOME
-
-echo
-sleep 3
 pkg - apt update -y;pkg - apt upgrade -y
-echo
-echo -e "Termux updated..."
-echo -e "Please wait..."
-sleep 3
-
 mkdir lizrat
 apt-get install ruby -y
-
+clear
 cd $HOME
 wget https://github.com/busyloop/lolcat/archive/master.zip
 unzip master.zip
 cd lolcat-master/bin
 gem install lolcat
-
+clear
 cd $HOME
 wget https://raw.githubusercontent.com/Hax4us/Apkmod/master/setup.sh
 bash setup.sh
