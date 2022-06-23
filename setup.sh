9daa0469616ba5969c7fc4b456f15fc6e00b8a18
•JINN• <95747858+alvinbaby@users.noreply.github.com>
2022-06-23 11:59:19 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ cp colors.properties ../;rm colors.properties
 wget https://raw.githubusercontent.com/alvinbaby/Terminal-In-Termux/main/alvinbaby/colors.properties
 clear
 cd $HOME
-pkg - apt update -y;pkg - apt upgrade -y
+pkg-apt update -y;pkg-apt upgrade -y
 mkdir lizrat
 apt-get install ruby -y
 clear
@@ -27,6 +27,8 @@ wget https://github.com/busyloop/lolcat/archive/master.zip
 unzip master.zip
 cd lolcat-master/bin
 gem install lolcat
+clear
+apt install nodejs -y && npm install -g npm@6.14.10
 clear
 cd $HOME
 wget https://raw.githubusercontent.com/Hax4us/Apkmod/master/setup.sh
